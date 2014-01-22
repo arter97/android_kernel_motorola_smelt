329e8c407da021f086d85d4b68e7648d71711ee3
Sai Gurrappadi <sgurrappadi@nvidia.com>
2014-01-21 16:41:37 -0800

    sched: Force sleep on consecutive sched_yields
    If a task sched_yields to itself continuously, force the task to
    sleep in sched_yield. This will lower the CPU load of this task
    thereby lowering the cpu frequency and improving power.
    
    Added a stat variable to track how many times we sleep due these
    consecutive sched_yields. Also added sysctl knobs to control the number
    of consecutive sched_yields before which the sleep kicks in and the
    duration fo the sleep in us.
    
    Bug 1424617
    
    Change-Id: Ie92412b8b900365816e17237fcbd0aac6e9c94ce
    Signed-off-by: Sai Gurrappadi <sgurrappadi@nvidia.com>
    Reviewed-on: http://git-master/r/358455
    Reviewed-by: Wen Yi <wyi@nvidia.com>
    Reviewed-by: Peter Zu <pzu@nvidia.com>
    GVS: Gerrit_Virtual_Submit
    Reviewed-by: Diwakar Tundlam <dtundlam@nvidia.com>

--- kernel/sched/sched.h
+++ kernel/sched/sched.h
@@ -536,6 +536,7 @@ struct rq {
 
 	/* sys_sched_yield() stats */
 	unsigned int yld_count;
+	unsigned int yield_sleep_count;
 
 	/* schedule() stats */
 	unsigned int sched_count;
@@ -1323,6 +1324,8 @@ extern void check_preempt_curr(struct rq *rq, struct task_struct *p, int flags);
 extern const_debug unsigned int sysctl_sched_time_avg;
 extern const_debug unsigned int sysctl_sched_nr_migrate;
 extern const_debug unsigned int sysctl_sched_migration_cost;
+extern const_debug unsigned int sysctl_sched_yield_sleep_duration;
+extern const_debug int sysctl_sched_yield_sleep_threshold;
 
 static inline u64 sched_avg_period(void)
 {
