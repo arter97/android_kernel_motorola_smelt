15a8149124c549d22a000139d2cf66133ff39434
Park Ju Hyung <qkrwngud825@gmail.com>
2018-07-08 20:29:09 +0900

    Add build tools
    Signed-off-by: Park Ju Hyung <qkrwngud825@gmail.com>

---
+++ ramdisk_fix_permissions.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+chmod 644 file_contexts
+chmod 644 se*
+chmod 755 *.sh
+chmod 644 *.rc
+chmod 750 init*
+chmod 640 fstab*
+chmod 600 default.prop
+chmod 770 cache
+chmod 500 config
+chmod 771 data
+chmod 755 dev
+chmod 755 proc
+chmod 755 bin
+chmod 755 bin/*
+chmod 750 sbin
+chmod 750 sbin/*
+chmod 751 storage
+chmod 755 sys
+chmod 755 system
