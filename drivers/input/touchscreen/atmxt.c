3a6ca89703f311f23461cc41c15b501b4348f926
Park Ju Hyung <qkrwngud825@gmail.com>
2018-07-13 01:39:33 +0900

    Revert "atmxt: remove expensive mutex locks on IRQ handler"
    This reverts commit ed7e276ed3ba5e8f40bde52020670d4ad296f674.
    
    Unfortunately, IRQ handler on atmxt races with other routines.
    
    Signed-off-by: Park Ju Hyung <qkrwngud825@gmail.com>

--- drivers/input/touchscreen/atmxt.c
+++ drivers/input/touchscreen/atmxt.c
@@ -1483,6 +1483,8 @@ static irqreturn_t atmxt_isr(int irq, void *handle)
 	int drv_state;
 	int ic_state;
 
+	mutex_lock(dd->mutex);
+
 	drv_state = atmxt_get_drv_state(dd);
 	ic_state = atmxt_get_ic_state(dd);
 
@@ -1545,6 +1547,7 @@ static irqreturn_t atmxt_isr(int irq, void *handle)
 	}
 
 	atmxt_dbg(dd, ATMXT_DBG3, "%s: IRQ Serviced.\n", __func__);
+	mutex_unlock(dd->mutex);
 
 	return IRQ_HANDLED;
 }
