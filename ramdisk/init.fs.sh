e0974d3eef9baee25f4b654b471199fb669c7180
Park Ju Hyung <qkrwngud825@gmail.com>
2020-03-02 20:29:17 +0900

    ramdisk: update swap settings
    Signed-off-by: Park Ju Hyung <qkrwngud825@gmail.com>

--- ramdisk/init.fs.sh
+++ ramdisk/init.fs.sh
@@ -5,9 +5,9 @@ busybox mount --bind /data/bind/system/framework /system/framework
 busybox mount --bind /data/bind/system/priv-app /system/priv-app
 
 # Setup swap here to avoid memory allocation errors
-# 256 MB
-echo $((256 * 1048576)) > /sys/devices/virtual/block/vnswap0/disksize
-echo 160 > /proc/sys/vm/swappiness
+# 512 MB
+echo $((512 * 1048576)) > /sys/devices/virtual/block/vnswap0/disksize
+echo 180 > /proc/sys/vm/swappiness
 busybox mkswap /dev/block/vnswap0
 busybox swapon /dev/block/vnswap0
 
