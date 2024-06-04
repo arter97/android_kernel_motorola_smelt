7e078c63a45f3a318c122e266155ea14edf352c3
Juhyung Park <qkrwngud825@gmail.com>
2024-06-04 22:07:48 +0900

    Revert "msm: Improve qpnp-vibrator scheduling"
    This reverts commit aa3a50d35ef39c8c487e639d0726656440beae52.
    
    Signed-off-by: Juhyung Park <qkrwngud825@gmail.com>

--- drivers/platform/msm/qpnp-vibrator.c
+++ drivers/platform/msm/qpnp-vibrator.c
@@ -65,7 +65,7 @@ struct qpnp_vib {
 	int state;
 	int vtg_level;
 	int timeout;
-	spinlock_t lock;
+	struct mutex lock;
 };
 
 static int qpnp_vib_read_u8(struct qpnp_vib *vib, u8 *data, u16 reg)
@@ -180,15 +180,9 @@ static void qpnp_vib_enable(struct timed_output_dev *dev, int value)
 {
 	struct qpnp_vib *vib = container_of(dev, struct qpnp_vib,
 					 timed_dev);
-	unsigned long flags;
 
-retry:
-	spin_lock_irqsave(&vib->lock, flags);
-	if (hrtimer_try_to_cancel(&vib->vib_timer) < 0) {
-		spin_unlock_irqrestore(&vib->lock, flags);
-		cpu_relax();
-		goto retry;
-	}
+	mutex_lock(&vib->lock);
+	hrtimer_cancel(&vib->vib_timer);
 
 	if (value == 0)
 		vib->state = 0;
@@ -200,9 +194,8 @@ retry:
 			      ktime_set(value / 1000, (value % 1000) * 1000000),
 			      HRTIMER_MODE_REL);
 	}
-	qpnp_vib_set(vib, vib->state);
-
-	spin_unlock_irqrestore(&vib->lock, flags);
+	mutex_unlock(&vib->lock);
+	schedule_work(&vib->work);
 }
 
 static void qpnp_vib_update(struct work_struct *work)
@@ -228,14 +221,9 @@ static enum hrtimer_restart qpnp_vib_timer_func(struct hrtimer *timer)
 {
 	struct qpnp_vib *vib = container_of(timer, struct qpnp_vib,
 							 vib_timer);
-	unsigned long flags;
-
-	spin_lock_irqsave(&vib->lock, flags);
 
 	vib->state = 0;
-	qpnp_vib_set(vib, vib->state);
-
-	spin_unlock_irqrestore(&vib->lock, flags);
+	schedule_work(&vib->work);
 
 	return HRTIMER_NORESTART;
 }
@@ -423,7 +411,7 @@ static int qpnp_vibrator_probe(struct spmi_device *spmi)
 		return rc;
 	}
 
-	spin_lock_init(&vib->lock);
+	mutex_init(&vib->lock);
 	INIT_WORK(&vib->work, qpnp_vib_update);
 
 	hrtimer_init(&vib->vib_timer, CLOCK_MONOTONIC, HRTIMER_MODE_REL);
@@ -451,6 +439,7 @@ static int qpnp_vibrator_remove(struct spmi_device *spmi)
 	cancel_work_sync(&vib->work);
 	hrtimer_cancel(&vib->vib_timer);
 	timed_output_dev_unregister(&vib->timed_dev);
+	mutex_destroy(&vib->lock);
 
 	return 0;
 }
